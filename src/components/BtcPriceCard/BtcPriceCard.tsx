677b39c41c4e6c2ac336627b2d69e0fd9e89f118
Andrei Lungu <andreilungu777@yahoo.ro>
2026-02-15 09:21:10 +0200

    Updated BtcPriceCard comp to use typography scale.

--- src/components/BtcPriceCard/BtcPriceCard.tsx
+++ src/components/BtcPriceCard/BtcPriceCard.tsx
@@ -14,8 +14,8 @@ function BtcNameSec() {
         <img id={styles.btcIcon} src={btcIcon} />
         <div id={styles.nameInnerContainer}>
             <div className={styles.nameRowOne}>
-                <p className={`${styles.nameStyle}`}>Bitcoin</p >
-                <p className={`${styles.symbolStyle}`}>BTC</p >
+                <p className={`${styles.name}`}>Bitcoin</p >
+                <p className={`${styles.symbol}`}>BTC</p >
             </div>
             <div className={styles.nameRowTwo}>
                 <div className={`${styles.metaBadge}`}>
@@ -40,7 +40,7 @@ function BtcPriceSec() {
 }
 
 function AnchBtn() {
-    return <button className={styles.seeChartsBtn}><span><svg data-slot="icon" fill="none" stroke-width="1" stroke="currentColor" width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    return <button id={styles.btnSeeCharts}><span><svg data-slot="icon" fill="none" stroke-width="1" stroke="currentColor" width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"></path>
     </svg></span>See Chart</button>
 }
