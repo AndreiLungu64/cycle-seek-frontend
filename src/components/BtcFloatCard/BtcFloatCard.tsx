49a2da603e381f5500289782ccd96c921a259d43
Andrei Lungu <andreilungu777@yahoo.ro>
2026-02-17 08:29:21 +0200

    Cleaned index.css and App.css centering logic.

--- src/components/BtcFloatCard/BtcFloatCard.tsx
+++ src/components/BtcFloatCard/BtcFloatCard.tsx
@@ -2,7 +2,6 @@ import btc3dIcon from "../../assets/BtcFloatCard/btc-3d-icon.png";
 import styles from "./BtcFloatCard.module.css";
 
 export function BtcFloatCard() {
-    return <div>
-        <img src={btc3dIcon} className={styles.icon} alt="Bitcoin 3D Icon" />
-    </div >
+    return <img src={btc3dIcon} className={styles.icon} alt="Bitcoin 3D Icon" />
+
 }
