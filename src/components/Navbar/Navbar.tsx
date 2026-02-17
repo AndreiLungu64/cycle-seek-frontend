2134a694c00cf30a96b1185b6dc31828f829016e
Andrei Lungu <andreilungu777@yahoo.ro>
2026-02-17 21:36:57 +0200

    Added links to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import styles from "./Navbar.module.css"
+import { Link } from "react-router-dom";
 
 export function Navbar() {
     return <div className={styles.navbarWrapper}>
-        <NavbarBtn text="Dashboard" />
-        <NavbarBtn text="Chart" />
-        <NavbarBtn text="Trades" />
+        <NavbarBtn text="Dashboard" path="/" />
+        <NavbarBtn text="Chart" path="/chart" />
+        <NavbarBtn text="Trades" path="/trades" />
     </div>
 }
 
-function NavbarBtn({ text }: { text: string }) {
-    return <button className={styles.btn}>{text}</button>
+function NavbarBtn({ text, path }: { text: string, path: string }) {
+    const selected = useState(false);
+    const btnState = selected ? "inactive" : "active";
+    return <button className={`${styles.btn} styles.${btnState}`}>
+        <Link to={path} className={`${styles.link}`}>{text}</Link>
+    </button >
+
 }
