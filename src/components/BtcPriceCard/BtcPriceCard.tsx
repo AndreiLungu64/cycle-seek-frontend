01c4db914b7b30a3bab18ae0d6524ef6af4988d2
Andrei Lungu <andreilungu777@yahoo.ro>
2026-02-19 12:01:55 +0200

    Finished search card static layout. Created btnBase utility class for buttons and applied it across the entire codebase.

--- src/components/BtcPriceCard/BtcPriceCard.tsx
+++ src/components/BtcPriceCard/BtcPriceCard.tsx
@@ -41,7 +41,8 @@ function BtcPriceSec() {
 }
 
 function AnchBtn() {
-    return <button className={styles.btnSeeCharts}><span><svg data-slot="icon" fill="none" stroke-width="1" stroke="currentColor" width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"></path>
-    </svg></span>See Chart</button>
+    return <button className={`${utils.btnBase} ${styles.btnSeeCharts}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="20" x2="12" y2="10"></line><line x1="18" y1="20" x2="18" y2="4"></line><line x1="6" y1="20" x2="6" y2="16"></line></svg>
+        <span> See Chart</span >
+    </button >
 }
